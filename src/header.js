d6d428728c508b85e8dca845e1212ae60e73cef3
Brunozo2042 <bruno_henriquemo@hotmail.com>
2024-04-26 10:05:11 -0300

    First component

---
+++ src/header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+
+export default function Header() {
+    return(
+        <header className='header'>
+            <h1>Bruno H.</h1>
+            <ul className='menu'>
+                <li>About</li>
+                <li>Buy</li>
+                <li>Contact</li>
+            </ul>
+        </header>
+    )
+}
+
